8edce7b6b991e0bd3efd156d42f2a02d1f991e7e
Andrei Fajilan <23-68657@g.batstate-u.edu.ph>
2025-11-19 10:38:00 +0800

    Delete forgot.js

--- forgot.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('forgotForm');
-const message = document.getElementById('successMessage');
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault(); // prevent page refresh
-  message.style.display = 'block'; // show success message
-  form.reset(); // clear input
-});
